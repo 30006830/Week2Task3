aa511226a822664ec870bd84252d32d70e8ab125
Blake Jensen <30006830@student.toiohomai.ac.nz>
2019-03-06 10:13:49 +1300

    Task 3 of W2 exercise

---
+++ Week2Task3/Week2Task3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //asks for the users first and last name and stores them in separate variables.
+            Console.WriteLine("Please enter your first name: ");
+            var fname = Console.ReadLine();
+
+            //asks for the last name of the user
+            Console.WriteLine("Please enter your last name: ");
+            var lname = Console.ReadLine();
+
+            //concatenates the 2 variables together to create 1 variable with both names joined.
+            var fullname = fname + " " + lname;
+            Console.WriteLine("Your name is {0}", fullname); //displays the users name using the variable "fullname" instead or "fname" and "lname"
+
+            Console.ReadLine();
+        }
+    }
+}
